b3bf78d4725195c1277a137aee090da2133adfa8
Ashishkumar-source <ashishlatiyan35@gmail.com>
2025-08-28 01:39:10 +0530

    Add files via upload

---
+++ Q1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+//input two number and display their sum
+int A,B;
+printf("enter A : ");
+scanf("%d",&A);
+printf("enter B : ");
+scanf("%d",&B);
+printf("sum is %d\n", A+B);
+
+}
