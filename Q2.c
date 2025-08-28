6b7f0f801f0d6ac89a1acf168aec0792fb1f9ca9
Ashishkumar-source <ashishlatiyan35@gmail.com>
2025-08-28 22:20:09 +0530

    Add files via upload

---
+++ Q2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+int a,b;
+printf("enter a : ");
+scanf("%d",&a);
+printf("enter b : ");
+scanf("%d",&b);
+printf("%d\n", a+b);
+printf("%d\n",a-b);
+printf("%d\n",a*b);
+printf("%d\n",a%b);
+return 0;
+}
